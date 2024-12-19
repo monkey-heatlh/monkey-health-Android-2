aafe2ad928f5477fe97725193dbba0c775090da5
jsw9436 <jswjsw101010@gmail.com>
2024-12-19 20:43:33 +0900

    ♻️ :: 불필요한 import문 삭제

--- presentaiton/src/main/java/com/example/presentaiton/Selection_Calendar/view/Selection_Calendar1.kt
+++ presentaiton/src/main/java/com/example/presentaiton/Selection_Calendar/view/Selection_Calendar1.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
