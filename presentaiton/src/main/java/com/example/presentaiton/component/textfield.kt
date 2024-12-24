fd5f7a363b2635d61e7b6594126a0055dc93476a
jsw9436 <jswjsw101010@gmail.com>
2024-12-24 12:03:42 +0900

    🚚  :: 파일 이름 수정

--- presentaiton/src/main/java/com/example/presentaiton/component/textfield.kt
+++ presentaiton/src/main/java/com/example/presentaiton/component/textfield.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -32,7 +31,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.presentaiton.view.Login.screen.Login2
 
 @Composable
 fun GomsTextField(
