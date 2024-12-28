e788c98f247ac73a91868b96d9641e2a65bf952c
jsw9436 <jswjsw101010@gmail.com>
2024-12-28 17:10:25 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/model/calender/response/GetRoutineModelResponse.kt
+++ domain/src/main/java/com/example/domain/model/calender/response/GetRoutineModelResponse.kt
@@ -1,4 +1,9 @@
 package com.example.domain.model.calender.response
 
-class GetRoutineModelResponse {
-}
+class GetRoutineModelResponse (
+    val mondayContent : String,
+    val tuesdayContent: String,
+    val wednesdayContent : String,
+    val thursdayContent: String,
+    val fridayContent : String
+)
