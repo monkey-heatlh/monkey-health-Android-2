e3142c2344f8ec008b3685465f4d518a5d984a16
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 01:15:50 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/di/RequestUrls.kt
+++ data/src/main/java/com/example/data/di/RequestUrls.kt
@@ -1,5 +1,13 @@
 package com.example.data.di
 
 object RequestUrls {
+    object AUTH {
+        private const val path = "api/auth"
 
+        const val auth = "$path/"
+        const val signUp = "$path/signup"
+        const val signIn = "$path/signin"
+        const val send = "$path/email/send"
+        const val verify = "$path/email/verify"
+    }
 }
