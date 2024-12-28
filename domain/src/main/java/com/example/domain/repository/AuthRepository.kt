0ed21e4bd1248552725e3c310afb4f8153a2e027
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 02:31:51 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,8 +1,10 @@
 package com.example.domain.repository
 
+import com.example.domain.model.auth.request.PostLoginRequestModel
 import java.util.concurrent.Flow
 
 interface AuthRepository {
     suspend fun signUp(body: signUpRequestModel): Flow <Unit>
 
+    suspend fun login(body: LoginRequestModel): Flow<Unit>
 }
