8b7b736a8e1dc54c6ce13183db82f1efb775a3d6
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 01:13:51 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/datasource/auth/AuthDataSource.kt
+++ data/src/main/java/com/example/data/datasource/auth/AuthDataSource.kt
@@ -1,13 +1,5 @@
 package com.example.data.datasource.auth
 
 interface AuthDataSource {
-    object AUTH {
-        private const val path = "api/auth"
 
-        const val auth = "$path/"
-        const val signUp = "$path/signup"
-        const val signIn = "$path/signin"
-        const val send = "$path/email/send"
-        const val verify = "$path/email/verify"
-    }
 }
