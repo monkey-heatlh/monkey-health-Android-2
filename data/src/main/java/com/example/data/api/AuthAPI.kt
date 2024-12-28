4abc64ecf56d51fe9fe2c5d0bf66a10853410577
jsw9436 <jswjsw101010@gmail.com>
2024-12-28 23:58:16 +0900

    ✨ :: 추가

--- data/src/main/java/com/example/data/api/AuthAPI.kt
+++ data/src/main/java/com/example/data/api/AuthAPI.kt
@@ -1,4 +1,13 @@
 package com.example.data.api
 
+import com.example.data.di.RequestUrls
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
 interface AuthAPI {
+    @POST(RequestUrls.AUTH.signUp)
+    suspend fun signUp(
+        @Body body: singUpRequest
+    )
 }
