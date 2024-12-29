7eabb30767326eae277da4986b9f4e314d40e114
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 23:51:45 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/response/auth/LoginResponse.kt
+++ data/src/main/java/com/example/data/dto/response/auth/LoginResponse.kt
@@ -1,5 +1,12 @@
 package com.example.data.dto.response.auth
 
-class LoginResponse {
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-}
+@JsonClass(generateAdapter = true)
+data class LoginResponse (
+    @Json(name = "accessToken") val accessToken: String,
+    @Json(name = "refreshToken") val refreshToken: String,
+    @Json(name = "accessTokenExp") val accessTokenExp: String,
+    @Json(name = "refreshTokenExp") val refreshTokenExp: String
+)
