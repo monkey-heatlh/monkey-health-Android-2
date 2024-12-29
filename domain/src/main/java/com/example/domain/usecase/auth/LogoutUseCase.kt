521d4308f0bce40b7edaaf6e872d5ca6e4c9c4c9
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 03:09:35 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/LogoutUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/LogoutUseCase.kt
@@ -1,4 +1,14 @@
 package com.example.domain.usecase.auth
 
-class LogoutUseCase {
+import com.example.domain.model.auth.request.LoginRequestModel
+import com.example.domain.model.auth.response.LoginResponseModel
+import com.example.domain.repository.AuthRepository
+import java.util.concurrent.Flow
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(body: LoginRequestModel): Flow<LoginResponseModel> =
+        authRepository.login(body = body)
 }
