3680e1b44929251668a6fc3fb388625c97b321ce
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 00:58:52 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/model/auth/request/LoginRequestModel.kt
+++ domain/src/main/java/com/example/domain/model/auth/request/LoginRequestModel.kt
@@ -1,6 +1,6 @@
 package com.example.domain.model.auth.request
 
-data class PostLoginRequestModel (
+data class LoginRequestModel (
     val email : String,
     val password : String,
 )
--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,11 +1,13 @@
 package com.example.domain.repository
 
+import com.example.domain.model.auth.request.LoginRequestModel
+import com.example.domain.model.auth.request.SignUpRequestModel
+import com.example.domain.model.auth.request.SendCodeRequestModel
 import com.example.domain.model.auth.response.LoginResponseModel
-import java.util.concurrent.Flow
-import kotlin.Unit as Unit
+import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun signUp(body: signUpRequestModel): Flow <Unit>
+    suspend fun signUp(body: SignUpRequestModel): Flow<Unit>
 
     suspend fun login(body: LoginRequestModel): Flow<Unit>
 
@@ -17,7 +19,7 @@ interface AuthRepository {
 
     suspend fun deleteToken()
 
-    suspend fun sendCode(body: sendCodeRequestModel): Flow<Unit>
+    suspend fun sendCode(body: SendCodeRequestModel): Flow<Unit>
 
     suspend fun verifyCode(email: String, code: String): Flow<Unit>
 
