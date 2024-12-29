2cbb217743d8ecacf8fb54dcc783a8d811225eda
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 13:59:37 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,6 +1,7 @@
 package com.example.domain.repository
 
 import com.example.domain.model.auth.request.PostLoginRequestModel
+import com.example.domain.model.auth.response.LoginResponseModel
 import java.util.concurrent.Flow
 
 interface AuthRepository {
@@ -9,4 +10,6 @@ interface AuthRepository {
     suspend fun login(body: LoginRequestModel): Flow<Unit>
 
     suspend fun tokenRefresh(refreshToken: String): Flow<Unit>
+
+    suspend fun saveToken(token: LoginResponseModel)
 }
