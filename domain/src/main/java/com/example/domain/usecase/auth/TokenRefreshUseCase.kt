a5bb3f0dd73b7c7d8cde29bdff811419ff8faadf
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 03:22:59 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/TokenRefreshUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/TokenRefreshUseCase.kt
@@ -1,4 +1,13 @@
 package com.example.domain.usecase.auth
 
-class TokenRefreshUseCase {
+import com.example.domain.model.auth.response.LoginResponseModel
+import com.example.domain.repository.AuthRepository
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+
+class TokenRefreshUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(refreshToken: String) : Flow<Unit> =
+        authRepository.tokenRefresh(refreshToken = refreshToken)
 }
