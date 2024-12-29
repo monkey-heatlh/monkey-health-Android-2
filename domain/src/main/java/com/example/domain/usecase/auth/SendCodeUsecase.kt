c79dcac601e4a001a10ed2a74e39d3a51c85a23c
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 03:29:08 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/SendCodeUsecase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/SendCodeUsecase.kt
@@ -1,4 +1,13 @@
-package com.example.domain.usecase.auth
+package com.goms.domain.auth
 
-class SendCodeUsecase {
+import com.example.domain.model.auth.response.LoginResponseModel
+import com.example.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class SaveCodeUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(token: LoginResponseModel) = kotlin.runCatching {
+        authRepository.saveToken(token = token)
+    }
 }
