b9a41c4a559525a50952d71d0880ba88caa64b9a
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 23:29:14 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/request/auth/SignUpRequest.kt
+++ data/src/main/java/com/example/data/dto/request/auth/SignUpRequest.kt
@@ -1,5 +1,10 @@
 package com.example.data.dto.request.auth
 
-data class SignUpRequest {
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-}
+@JsonClass(generateAdapter = true)
+data class SignUpRequest(
+    @Json(name = "email") val email: String,
+    @Json(name = "password") val password: String
+)
