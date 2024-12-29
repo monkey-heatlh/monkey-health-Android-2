4509169bc435d96ba5690bd679dae714d9bda7b9
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 03:28:24 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/LoginUsecase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/LoginUsecase.kt
@@ -1,4 +1,17 @@
-package com.example.domain.usecase.auth
+package com.goms.domain.auth
 
-class LoginUsecase {
+import com.example.domain.model.auth.request.LoginRequestModel
+import com.example.domain.model.auth.response.LoginResponseModel
+import com.example.domain.repository.AuthRepository
+import com.goms.data.repository.auth.AuthRepository
+import com.goms.model.request.auth.LoginRequestModel
+import com.goms.model.response.auth.LoginResponseModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(body: LoginRequestModel): Flow<LoginResponseModel> =
+        authRepository.login(body = body)
 }
