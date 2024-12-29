3a5315225f5bde87ce0c3df0530c7a6f347105f9
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 03:34:47 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/SignUpUsecase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/SignUpUsecase.kt
@@ -1,4 +1,13 @@
 package com.example.domain.usecase.auth
 
-class SignUpUsecase {
+import com.example.domain.model.auth.request.SignUpRequestModel
+import com.example.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class SighUpUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(body: SignUpRequestModel) = kotlin.runCatching {
+        authRepository.signUp(body = body)
+    }
 }
