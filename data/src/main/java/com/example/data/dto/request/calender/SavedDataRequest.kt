9aa7fbe49e6ec505602ca5139d2a3796669de009
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 08:58:46 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/request/calender/SavedDataRequest.kt
+++ data/src/main/java/com/example/data/dto/request/calender/SavedDataRequest.kt
@@ -1,4 +1,12 @@
 package com.example.data.dto.request.calender
 
-class SavedDataRequest {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SavedDataRequest (
+    @Json(name = "id") val id: String,
+    @Json(name = "email") val email: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "data") val data: String
+)
