ae299d96be9776152914178c3fc0adc772194e1a
jsw9436 <jswjsw101010@gmail.com>
2024-12-29 23:54:33 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/datasource/auth/AuthDataSource.kt
+++ data/src/main/java/com/example/data/datasource/auth/AuthDataSource.kt
@@ -1,8 +1,12 @@
 package com.example.data.datasource.auth
 
+import com.example.data.dto.request.auth.LoginRequest
 import com.example.data.dto.request.auth.SignUpRequest
+import com.example.data.dto.response.auth.LoginResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
     suspend fun signUp(body: SignUpRequest): Flow<Unit>
+
+    suspend fun login(body: LoginRequest): Flow<LoginResponse>
 }
