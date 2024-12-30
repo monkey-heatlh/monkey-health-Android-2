284db8443101e0c8e0fbbf5f27b91c4655d47e1e
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 22:53:43 +0900

    ✨ :: 추가

--- data/src/main/java/com/example/data/datasource/Calender/CalenderDataSource.kt
+++ data/src/main/java/com/example/data/datasource/Calender/CalenderDataSource.kt
@@ -1,4 +1,7 @@
 package com.example.data.datasource.Calender
 
+import com.example.data.dto.response.calender.GetCalenderResponse
+
 interface CalenderDataSource {
+    suspend fun getDateCalender(): Flow<List<GetCalenderResponse>>
 }
