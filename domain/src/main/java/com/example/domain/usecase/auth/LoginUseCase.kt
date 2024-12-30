94c5b77d109d26de44d5049ff5d66c365a76c79a
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 09:52:50 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/LoginUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/LoginUseCase.kt
@@ -3,9 +3,6 @@ package com.example.domain.usecase.auth
 import com.example.domain.model.auth.request.LoginRequestModel
 import com.example.domain.model.auth.response.LoginResponseModel
 import com.example.domain.repository.AuthRepository
-import com.goms.data.repository.auth.AuthRepository
-import com.goms.model.request.auth.LoginRequestModel
-import com.goms.model.response.auth.LoginResponseModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
