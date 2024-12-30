1636849c55c2e415f2e952dabc788e5604a54135
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 16:12:30 +0900

    🔥 :: 삭제

--- domain/src/main/java/com/example/domain/model/calender/request/GetRoutineModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.model.calender.request
-
-data class GetRoutineModel (
-    val mondayContent: String,
-    val tuesdayContent: String,
-    val wednesdayContent: String,
-    val thursdayContent: String,
-    val fridayContent: String
-)
