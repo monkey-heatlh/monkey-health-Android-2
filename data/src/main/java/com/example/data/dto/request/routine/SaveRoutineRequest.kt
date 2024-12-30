24fd4d96b3b56f5df23d33f910d2b7d99878a4a5
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 09:21:09 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/request/routine/SaveRoutineRequest.kt
+++ data/src/main/java/com/example/data/dto/request/routine/SaveRoutineRequest.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class SaveRoutineRequest (
-    @Json(name = "monday_content") val monday_content : String,
+    @Json(name = "monday_content") val monday_content: String,
     @Json(name = "tuesday_content") val tuesday_content: String,
     @Json(name = "wednesday_content") val wednesday_content: String,
     @Json(name = "thursday_content") val thursday_content: String,
