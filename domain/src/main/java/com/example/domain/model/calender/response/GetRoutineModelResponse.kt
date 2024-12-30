795826c14d23273de7993096e5a234e11a5310ed
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 16:17:32 +0900

    🔥 :: 잘못된 파일 삭제

--- domain/src/main/java/com/example/domain/model/calender/response/GetRoutineModelResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.model.calender.response
-
-class GetRoutineModelResponse (
-    val mondayContent : String,
-    val tuesdayContent: String,
-    val wednesdayContent : String,
-    val thursdayContent: String,
-    val fridayContent : String
-)
