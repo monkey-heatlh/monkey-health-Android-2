3948b04273e395d8b5dad690777405b57f72a1e3
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 23:25:42 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/auth/TokenRefreshUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/TokenRefreshUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.domain.usecase.auth
 
-import com.example.domain.model.auth.response.LoginResponseModel
 import com.example.domain.repository.AuthRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
