36aa02d16a482a5e3d38a7a3be304a559aa2eb58
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 22:56:08 +0900

    📦 :: 파일 이름 수정

--- data/src/main/java/com/example/data/dto/response/calender/CalenderResponse.kt
+++ data/src/main/java/com/example/data/dto/response/calender/CalenderResponse.kt
@@ -4,7 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class GetCalenderResponse (
+data class CalenderResponse (
     @Json(name = "id") val id: String,
     @Json(name = "email") val email: String,
     @Json(name = "content") val content: String,
