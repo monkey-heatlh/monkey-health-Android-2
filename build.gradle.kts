28fbf3bb237c4761a9063e07973f0f8657d6d18f
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 19:37:50 +0900

    ✨ :: 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
-    id(Dependency.Gradle.APPLICATION) version Versions.GRADLE apply false
-    id(Dependency.Gradle.LIBRARY) version Versions.GRADLE apply false
-    id(Dependency.Gradle.KOTLIN) version Versions.KOTLIN apply false
-    id(Dependency.Hilt.HILT_PLUGIN) version Versions.HILT apply false
-    id(Dependency.Google.GOOGLE_SERVICES_PLUGIN) version Versions.GOOGLE_SERVICE_PLUGIN apply false
-    id(Dependency.Gradle.KSP) version Versions.KSP apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
--- data/src/main/java/com/example/data/util/TokenAuthenticator.kt
+++ data/src/main/java/com/example/data/util/TokenAuthenticator.kt
@@ -1,5 +1,6 @@
 package com.example.data.util
 
+import com.goms.datastore.datasource.auth.AuthTokenDataSource
 import javax.inject.Inject
 
 class TokenAuthenticator @Inject constructor(
