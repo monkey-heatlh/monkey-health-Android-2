05f0afdbe68972f683dd55ed6d85ac3fb9b1818e
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 09:14:26 +0900

    ♻️ :: 코드 리팩토리

--- data/src/main/java/com/example/data/dto/response/calender/GetCalenderResponse.kt
+++ data/src/main/java/com/example/data/dto/response/calender/GetCalenderResponse.kt
@@ -1,4 +1,12 @@
 package com.example.data.dto.response.calender
 
-class GetCalenderResponse {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+class GetCalenderResponse (
+    @Json(name = "id") val id: String,
+    @Json(name = "email") val email: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "data") val data: String
+    )
