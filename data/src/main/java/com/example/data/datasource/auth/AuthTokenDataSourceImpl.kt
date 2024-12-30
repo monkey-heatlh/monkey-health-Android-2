c4eae1ca51ddff1a609dd7541731f709766542bd
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 23:29:22 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/datasource/auth/AuthTokenDataSourceImpl.kt
+++ data/src/main/java/com/example/data/datasource/auth/AuthTokenDataSourceImpl.kt
@@ -1,4 +1,110 @@
-package com.example.data.datasource.auth
+package com.goms.datastore.datasource.auth
 
-class AuthTokenDataSourceImpl {
+import androidx.datastore.core.DataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AuthTokenDataSourceImpl @Inject constructor(
+    private val authToken: DataStore<AuthToken>
+) : AuthTokenDataSource {
+    override fun getAccessToken(): Flow<String> = authToken.data.map {
+        it.accessToken ?: ResourceKeys.EMPTY
+    }
+
+    override suspend fun setAccessToken(accessToken: String) {
+        authToken.updateData {
+            it.toBuilder()
+                .setAccessToken(accessToken)
+                .build()
+        }
+    }
+
+    override suspend fun removeAccessToken() {
+        authToken.updateData {
+            it.toBuilder()
+                .clearAccessToken()
+                .build()
+        }
+    }
+
+    override fun getAccessTokenExp(): Flow<String> = authToken.data.map {
+        it.accessExp ?: ResourceKeys.EMPTY
+    }
+
+    override suspend fun setAccessTokenExp(accessTokenExp: String) {
+        authToken.updateData {
+            it.toBuilder()
+                .setAccessExp(accessTokenExp)
+                .build()
+        }
+    }
+
+    override suspend fun removeAccessTokenExp() {
+        authToken.updateData {
+            it.toBuilder()
+                .clearAccessExp()
+                .build()
+        }
+    }
+
+    override fun getRefreshToken(): Flow<String> = authToken.data.map {
+        it.refreshToken ?: ResourceKeys.EMPTY
+    }
+
+    override suspend fun setRefreshToken(refreshToken: String) {
+        authToken.updateData {
+            it.toBuilder()
+                .setRefreshToken(refreshToken)
+                .build()
+        }
+    }
+
+    override suspend fun removeRefreshToken() {
+        authToken.updateData {
+            it.toBuilder()
+                .clearRefreshToken()
+                .build()
+        }
+    }
+
+    override fun getRefreshTokenExp(): Flow<String> = authToken.data.map {
+        it.refreshExp ?: ResourceKeys.EMPTY
+    }
+
+    override suspend fun setRefreshTokenExp(refreshTokenExp: String) {
+        authToken.updateData {
+            it.toBuilder()
+                .setRefreshExp(refreshTokenExp)
+                .build()
+        }
+    }
+
+    override suspend fun removeRefreshTokenExp() {
+        authToken.updateData {
+            it.toBuilder()
+                .clearRefreshExp()
+                .build()
+        }
+    }
+
+    override fun getAuthority(): Flow<String> = authToken.data.map {
+        it.authority ?: ResourceKeys.EMPTY
+    }
+
+    override suspend fun setAuthority(authority: String) {
+        authToken.updateData {
+            it.toBuilder()
+                .setAuthority(authority)
+                .build()
+        }
+    }
+
+    override suspend fun removeAuthority() {
+        authToken.updateData {
+            it.toBuilder()
+                .clearAuthority()
+                .build()
+        }
+    }
 }
