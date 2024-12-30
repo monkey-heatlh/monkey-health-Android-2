872ab2e7a2510405dd04cacf3dc7d00f403279ad
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 09:03:09 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/request/routine/SaveRoutineRequest.kt
+++ data/src/main/java/com/example/data/dto/request/routine/SaveRoutineRequest.kt
@@ -1,4 +1,13 @@
 package com.example.data.dto.request.routine
 
-class SaveRoutineRequest {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SaveRoutineRequest (
+    @Json(name = "monday_content") val monday_content : String,
+    @Json(name = "tuesday_content") val tuesday_content: String,
+    @Json(name = "wednesday_content") val wednesday_content: String,
+    @Json(name = "thursday_content") val thursday_content: String,
+    @Json(name = "friday_content") val friday_content: String
+)
