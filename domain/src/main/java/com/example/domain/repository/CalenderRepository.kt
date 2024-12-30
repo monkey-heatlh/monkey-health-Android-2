98963e804d9da382521f6e1b13177a02c04dcc28
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 23:06:56 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/repository/CalenderRepository.kt
+++ domain/src/main/java/com/example/domain/repository/CalenderRepository.kt
@@ -1,4 +1,8 @@
 package com.example.domain.repository
 
+import com.example.domain.model.calender.request.SaveCalenderRequestModel
+import java.util.concurrent.Flow
+
 interface CalenderRepository {
+    fun dataCalender(body: SaveCalenderRequestModel): Flow<Unit>
 }
