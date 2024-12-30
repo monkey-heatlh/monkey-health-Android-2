a2202ccf0d540c86d8a39c65d9937513ec488e82
jsw9436 <jswjsw101010@gmail.com>
2024-12-30 09:20:46 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/dto/response/ai/aiResponse.kt
+++ data/src/main/java/com/example/data/dto/response/ai/aiResponse.kt
@@ -1,5 +1,12 @@
 package com.example.data.dto.response.ai
 
-class aiResponse {
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
-}
+@JsonClass(generateAdapter = true)
+data class aiResponse (
+    @Json(name = "angle") val angle: String,
+    @Json(name = "state") val state: String,
+    @Json(name = "post_angle") val post_angle: String,
+    @Json(name = "ready_state") val ready_state: String
+)
