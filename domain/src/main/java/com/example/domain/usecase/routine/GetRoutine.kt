8fcfadf28ccda6166646a8579cccf7bb39d28264
jsw9436 <jswjsw101010@gmail.com>
2024-12-31 13:46:06 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/usecase/routine/GetRoutine.kt
+++ domain/src/main/java/com/example/domain/usecase/routine/GetRoutine.kt
@@ -1,4 +1,14 @@
 package com.example.domain.usecase.routine
 
-class GetRoutine {
+import com.example.domain.model.auth.request.LoginRequestModel
+import com.example.domain.model.routine.request.PostRoutineModel
+import com.example.domain.repository.AuthRepository
+import com.example.domain.repository.RoutineRepository
+import javax.inject.Inject
+
+class GetRoutine @Inject constructor(
+    private val RoutineRepository: RoutineRepository
+) {
+    suspend operator fun invoke(body: PostRoutineModel) =
+        PostRoutineModel.post(body = body)
 }
