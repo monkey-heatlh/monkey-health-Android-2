5d726b600c384087ecf9178d7218aa3d1abd6bb7
jsw9436 <jswjsw101010@gmail.com>
2025-01-03 23:33:54 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/model/calender/request/SaveCalenderRequestModel.kt
+++ domain/src/main/java/com/example/domain/model/calender/request/SaveCalenderRequestModel.kt
@@ -2,7 +2,7 @@ package com.example.domain.model.calender.request
 
 class SaveCalenderRequestModel (
     val id: String,
-    val eamil: String,
+    val email: String,
     val content: String,
     val date: String
     )
