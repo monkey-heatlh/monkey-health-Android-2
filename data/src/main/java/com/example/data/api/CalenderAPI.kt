3a864669c26560c080d1d1ca642bf82a8cefead5
jsw9436 <jswjsw101010@gmail.com>
2025-01-03 23:52:38 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/api/CalenderAPI.kt
+++ data/src/main/java/com/example/data/api/CalenderAPI.kt
@@ -1,12 +1,18 @@
 package com.example.data.api
 
 import com.example.data.di.RequestUrls
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 
 interface CalenderAPI {
     @GET(RequestUrls.CALENDER.date)
     suspend fun date(
+
+    )
+    @POST(RequestUrls.CALENDER.save)
+    suspend fun save(
 
     )
 }
