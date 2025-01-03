37019b050ab75aa2dd32239e3b0cf14db5c4c8dc
jsw9436 <jswjsw101010@gmail.com>
2025-01-03 23:40:16 +0900

    ♻️ :: 코드 리팩토링

--- domain/src/main/java/com/example/domain/model/calender/response/GetCalenderResponseModel.kt
+++ domain/src/main/java/com/example/domain/model/calender/response/GetCalenderResponseModel.kt
@@ -2,5 +2,7 @@ package com.example.domain.model.calender.response
 
 class GetCalenderResponseModel (
     val id: String,
-
+    val email: String,
+    val content: String,
+    var date: String
 )
