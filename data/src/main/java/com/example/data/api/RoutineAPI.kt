e7cd8f6d0bfca82e205d34070a6f50165b32ec2d
jsw9436 <jswjsw101010@gmail.com>
2025-01-04 16:53:39 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/api/RoutineAPI.kt
+++ data/src/main/java/com/example/data/api/RoutineAPI.kt
@@ -2,11 +2,15 @@ package com.example.data.api
 
 import com.example.data.di.RequestUrls
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface RoutineAPI {
     @GET(RequestUrls.ROUTINE.read)
     suspend fun readRoutine(
 
     )
+    @POST(RequestUrls.ROUTINE.save)
+    suspend fun saveRoutine(
 
+    )
 }
