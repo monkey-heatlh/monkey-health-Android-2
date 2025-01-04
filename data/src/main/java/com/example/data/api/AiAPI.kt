9d986f939a4a94f26e78a468fae755495fe3da66
jsw9436 <jswjsw101010@gmail.com>
2025-01-05 02:37:03 +0900

    ♻️ :: 코드 리팩토링

--- data/src/main/java/com/example/data/api/AiAPI.kt
+++ data/src/main/java/com/example/data/api/AiAPI.kt
@@ -1,4 +1,11 @@
 package com.example.data.api
 
+import com.example.data.di.RequestUrls
+import retrofit2.http.POST
+
 interface AiAPI {
+    @POST(RequestUrls.AI.analyzePushup)
+    suspend fun analyzePushup(
+
+    )
 }
